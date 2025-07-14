f63affa1241e69cf1cf8453f90a54c0fc116f5d8
Mohan kumar <pmohankumar550@gmail.com>
2025-07-14 21:04:32 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        ApplicationContext n1= new ClassPathXmlApplicationContext("springconfig.xml");
+        System.out.println( "Hello World!" );
+
+        Student student=(Student) n1.getBean("st1");
+        student.show();
+
+        
+ 
+    }
+}
---
+++ src/main/java/com/example/Student.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Student {
+
+    // public Student(){
+    //     System.out.println("studen is created");
+    // }
+
+    public void  show(){
+        System.out.println("show is working");
+
+    }
+    
+}
